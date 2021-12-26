379e82f1f6131ad35ae588acb42b590ace09e421
duxiuyulin <77013695+duxiuyulin@users.noreply.github.com>
2021-12-26 12:39:25 +0800

    tom-弟弟果园依赖

---
+++ wool/ddgyCOOKIE.js
@@ -0,0 +1,86 @@
+module.exports = {
+    "id": "ddgy",
+    "name": "弟弟果园",
+    "keys": ["ddgyurl","ddgyheader","ddgyurl2","ddgyheader2","ddgyurl3","ddgyheader3","ddgyurl4","ddgyheader4","ddgyurl5","ddgyheader5","ddgyurl6","ddgyheader6","ddgyurl7","ddgyheader7","ddgyurl8","ddgyheader8","ddgyurl9","ddgyheader9","ddgyurl10","ddgyheader10","ddgyurl11","ddgyheader11","ddgyurl12","ddgyheader12","ddgyurl13","ddgyheader13","ddgyurl14","ddgyheader14","ddgyurl15","ddgyheader15"],
+    "author": "@tom",
+    "settings": [{
+      "id": "ddgySuffix",
+      "name": "当前账号",
+      "val": "1",
+      "type": "number",
+      "desc": "当前抓取ck记录的账号序号，如：1、2、3、4"
+    }, {
+      "id": "ddgyCount",
+      "name": "账号个数",
+      "val": "1",
+      "type": "number",
+      "desc": "指定任务最多跑几个账号，根据抓取的账号数据个数来设值"
+    }, {
+      "id": "ddgyXH",
+      "name": "循环获取ck",
+      "val": "0",
+      "type": "number",
+      "desc": "0关闭，1打开，默认关闭"
+    }, {
+      "id": "ddgyTXTX",
+      "name": "txtx",
+      "val": "0",
+      "type": "number",
+      "desc": "0关闭，1打开，默认关闭"
+    }, {
+      "id": "ddgySC",
+      "name": "sc",
+      "val": "0",
+      "type": "number",
+      "desc": "0关闭，1打开，默认关闭"
+    }, {
+      "id": "ddgynotifyttt",
+      "name": "推送控制",
+      "val": "1",
+      "type": "number",
+      "desc": "0关闭，1推送,默认12点以及23点推送"
+    }, {
+      "id": "ddgynotifyInterval",
+      "name": "通知控制",
+      "val": "2",
+      "type": "number",
+      "desc": "0关闭，1为 所有通知，2为 12，23 点通知，3为 6，12，18，23 点通知"
+    }, {
+      "id": "ddgyMinutes",
+      "name": "推送-通知 分钟控制",
+      "val": "10",
+      "type": "number",
+      "desc": "推送以及通知控制在什么分钟段，可设置0-59,默认0到10"
+    }],
+      "repo": "https://raw.githubusercontent.com/xl2101200/-/main/ddgy.js",
+    "icons": ["https://raw.githubusercontent.com/xl2101200/-/main/tom/tom.jpg", "https://raw.githubusercontent.com/xl2101200/-/main/tom/tom.jpg"],
+    "script": "https://raw.githubusercontent.com/xl2101200/-/main/ddgy.js",
+    "icon": "https://raw.githubusercontent.com/xl2101200/-/main/tom/tom.jpg",
+    "favIcon": "mdi-star-outline",
+    "favIconColor": "grey",
+    "datas": [{
+      "key": "ddgyurl",
+      "val": "dd03-O5b5QTbXSIofxvWbm8fumZjiyPNaYo8InK8Ps1JlyPN9xQRBVpmwmPDUp2o9x4sKhR4/lZgtpYtcRNogluKTkP3/uPu9Oon1m+cpkSJlpIjDz4mJk3CZmZJiuPU"
+    }, {
+      "key": "ddgyheader",
+      "val": "c6aoWqWIfvP4UHHLWwFW6xe89doSHS6K8jSeimD3pqskzDdqxlAQAOG7TL2IDS-s9jYOcmiewcaV0N2Nf1UDU3wnSyli000RllEmLKeiq-7CCspm33302T0j_1-jVFidAuHpzjPl2bK5-5wRrQ3h9cEd1MnP1-_3y0F1Vd0v4Y2yEZZpLafwTmFpQ2cL80D4uNlPSq-_AAAA__8="
+    }, {
+      "key": "ddgyurl2",
+      "val": ""
+    }, {
+      "key": "ddgyheader2",
+      "val": ""
+    }
+    //多账号自行复制以下注释内容
+    //,{
+     // "key": "ddgyurl",
+     // "val": ""
+   // }, {
+    //  "key": "ddgyheader",
+     // "val": ""
+    //}
+],
+    "sessions": [],
+    "isLoaded": true
+  }
+  
