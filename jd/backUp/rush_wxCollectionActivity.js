2e3f33f601b70c97300cc2acbd41832d968e3863
duxiuyulin <77013695+duxiuyulin@users.noreply.github.com>
2022-01-21 22:12:21 +0800

    不跳过null

--- jd/backUp/rush_wxCollectionActivity.js
+++ jd/backUp/rush_wxCollectionActivity.js
@@ -1,19 +1,23 @@
 /*
 https://lzkj-isv.isvjcloud.com/wxgame/activity/8530275?activityId=
+
+TG https://t.me/duckjobs
+
+不能并发
+
+JD_CART_REMOVESIZE || 20; // 运行一次取消多全部已关注的商品。数字0表示不取关任何商品
+JD_CART_REMOVEALL || true;    //是否清空，如果为false，则上面设置了多少就只删除多少条
+7 7 7 7 * jd_wxCollectionActivity.js
 */
 const $ = new Env('加购物车抽奖');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const notify = $.isNode() ? require('./sendNotify') : '';
-let cookiesArr = [], cookie = '', message = '';
-let activityIdList = [
-    'e19e4296e1404a62afd6e3da2f44adec',
-    'a898cd1be33d4945897d43667c58b7c0',
-    '3b071ff4c2d04dd4a9e56250049835d6',
-]
+let cookiesArr = [], cookie = '', message = '' ;
+let activityIdList = []
 let lz_cookie = {}
 
-if (process.env.ACTIVITY_ID && process.env.ACTIVITY_ID != "") {
-    activityId = process.env.ACTIVITY_ID;
+if (process.env.DDO && process.env.DDO != "") {
+    activityId = process.env.DDO.split('\n');
 }
 
 if ($.isNode()) {
@@ -30,6 +34,7 @@ if ($.isNode()) {
     cookiesArr.reverse();
     cookiesArr = cookiesArr.filter(item => !!item);
 }
+let doPush = process.env.DoPush || false; // 设置为 false 每次推送, true 跑完了推送
 let removeSize = process.env.JD_CART_REMOVESIZE || 20; // 运行一次取消多全部已关注的商品。数字0表示不取关任何商品
 let isRemoveAll = process.env.JD_CART_REMOVEALL || true;    //是否清空，如果为false，则上面设置了多少就只删除多少条
 $.keywords = process.env.JD_CART_KEYWORDS || []
@@ -62,7 +67,7 @@ $.keywordsNum = 0;
                     continue
                 }
                 authorCodeList = [
-                    'b5d9535918264a4f92fff9d314d7db81',
+                    '',
                 ]
                 $.bean = 0;
                 $.ADID = getUUID('xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', 1);
@@ -75,11 +80,11 @@ $.keywordsNum = 0;
                 $.getPrize = null;
                 await addCart();
                 if($.drawInfoName === false || $.getPrize === null){
-                    break
+                    //break
                 } else if($.getPrize != null && !$.getPrize.includes("京豆")){
                     break
                 }
-                await $.wait(3000)
+                await $.wait(2000)
                 await requireConfig();
                 do {
                     await getCart_xh();
@@ -132,11 +137,11 @@ async function addCart() {
             if ($.activityContent.drawInfo.name.includes("京豆")) {
                 $.log("-> 加入购物车")
                 for(let i in $.activityContent.cpvos){
-                    await $.wait(3000)
+                    await $.wait(1000)
                     await task('addCart', `activityId=${$.activityId}&pin=${encodeURIComponent($.secretPin)}&productId=${$.activityContent.cpvos[i].skuId}`)
                 }
                 $.log("-> 抽奖")
-                await $.wait(3000)
+                await $.wait(1000)
                 await task('getPrize', `activityId=${$.activityId}&pin=${encodeURIComponent($.secretPin)}`)
             } else {
                 $.log("未能成功获取到活动信息")
@@ -185,7 +190,13 @@ function task(function_id, body, isCommon = 0) {
                                 case 'getPrize':
                                     console.log(data.data.name)
                                     $.getPrize = data.data.name;
-                                    await notify.sendNotify($.name, data.data.name, '', `\n`);
+                                    if (doPush === true) {
+                                        if (data.data.name) {
+                                            message += data.data.name + " "
+                                        }
+                                    } else {
+                                        // await notify.sendNotify($.name, data.data.name, '', `\n`);
+                                    }
                                     break
                                 default:
                                     $.log(JSON.stringify(data))
@@ -193,7 +204,7 @@ function task(function_id, body, isCommon = 0) {
                             }
                         }
                     } else {
-                        $.log("京东没有返回数据")
+                        // $.log("京东没有返回数据")
                     }
                 }
             } catch (error) {
@@ -348,6 +359,10 @@ function random(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 
 }
+function strToJson(str){
+	var json = eval('(' + str + ')');
+	return json;
+}
 function requireConfig(){
     return new Promise(resolve => {
         if($.isNode() && process.env.JD_CART){
@@ -370,7 +385,7 @@ function getCart_xh(){
         }
         $.get(option, async(err, resp, data) => {
             try{
-                data = JSON.parse(getSubstr(data, "window.cartData = ", "window._PFM_TIMING"));
+                data = strToJson(getSubstr(data, "window.cartData = ", "window._PFM_TIMING"));
                 $.areaId = data.areaId;   // locationId的传值
                 $.traceId = data.traceId; // traceid的传值
                 venderCart = data.cart.venderCart;
